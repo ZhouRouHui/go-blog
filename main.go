12718345272cf5c1645f8e380dd3d39fd93b04b3
zhourouhui <zhourouhui@ahaschool.com>
2020-11-25 10:30:55 +0800

    显示文章

--- main.go
+++ main.go
@@ -43,11 +43,42 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
 }
 
+// Article 文章
+type Article struct {
+	Title, Body string
+	ID          int64
+}
+
 // articlesShowHandler 文章详情
 func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
+	// 1. 获取 url 参数
 	vars := mux.Vars(r) // Vars() 返回当前请求的路由变量
 	id := vars["id"]
-	fmt.Fprint(w, "文章 ID："+id)
+
+	// 2. 读取对应的文章数据
+	article := Article{}
+	query := "SELECT * FROM articles WHERE id = ?"
+	err := db.QueryRow(query, id).Scan(&article.ID, &article.Title, &article.Body)
+
+	// 3. 如果出现问题
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// 3.1 数据未找到
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprint(w, "404 文章未找到")
+		} else {
+			// 3.2 数据库错误
+			checkError(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "500 服务器内部错误")
+		}
+	} else {
+		// 4. 读取成功
+		tmpl, err := template.ParseFiles("resources/views/articles/show.gohtml")
+		checkError(err)
+
+		tmpl.Execute(w, article)
+	}
 }
 
 // articlesIndexHandler 文章列表
