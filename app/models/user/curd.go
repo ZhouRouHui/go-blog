97709fe20fb17208e5e462eee4d860e2aa26d72c
zhourouhui <zhourouhui@ahaschool.com>
2020-12-07 14:52:58 +0800

    会话和认证管理

--- app/models/user/curd.go
+++ app/models/user/curd.go
@@ -3,13 +3,36 @@ package user
 import (
 	"goblog/pkg/logger"
 	"goblog/pkg/model"
+	"goblog/pkg/types"
 )
 
 // Create 创建用户，通过 User.ID 来判断是否创建成功
-func (user *User) Create() (err error) {
-	if err = model.DB.Create(&user).Error; err != nil {
+func (u *User) Create() (err error) {
+	if err = model.DB.Create(&u).Error; err != nil {
 		logger.LogError(err)
 		return err
 	}
 	return nil
+}
+
+// Get 获取用户
+func Get(uid string) (user User, err error) {
+	id := types.StringToInt(uid)
+	if err = model.DB.First(&user, id).Error; err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+// GetByEmail 通过 email 获取用户
+func GetByEmail(email string) (user User, err error) {
+	if err = model.DB.Where("email = ?", email).First(&user).Error; err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+// ComparePassword 比对密码是否正确
+func (u User) ComparePassword(password string) bool {
+	return u.Password == password
 }
