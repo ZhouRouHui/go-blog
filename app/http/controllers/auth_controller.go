13345d187c4eaa493ab38feda5db1429acf31baa
zhourouhui <zhourouhui@ahaschool.com>
2020-12-07 09:59:56 +0800

    注册表单验证

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -1,11 +1,14 @@
 package controllers
 
 import (
+	"fmt"
+	"goblog/app/models/user"
+	"goblog/app/requests"
 	"goblog/pkg/view"
 	"net/http"
 )
 
-type AuthController struct {}
+type AuthController struct{}
 
 // Register 注册页面
 func (*AuthController) Register(w http.ResponseWriter, r *http.Request) {
@@ -14,5 +17,30 @@ func (*AuthController) Register(w http.ResponseWriter, r *http.Request) {
 
 // DoRegister 注册保存
 func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
-	//
+	// 1. 初始化数据
+	_user := user.User{
+		Name:            r.PostFormValue("name"),
+		Email:           r.PostFormValue("email"),
+		Password:        r.PostFormValue("password"),
+		PasswordConfirm: r.PostFormValue("password_confirm"),
+	}
+	// 2. 表单规则
+	errs := requests.ValidateRegistrationForm(_user)
+	if len(errs) > 0 {
+		// 3. 有错误发生，打印数据
+		view.RenderSimple(w, view.D{
+			"Errors": errs,
+			"User":   _user,
+		}, "auth.register")
+	} else {
+		// 4. 验证成功，创建数据
+		_user.Create()
+
+		if _user.ID > 0 {
+			http.Redirect(w, r, "/", http.StatusFound)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "注册失败，请联系管理员")
+		}
+	}
 }
